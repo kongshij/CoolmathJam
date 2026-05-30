4879d12910ca68295815b3d24ad9eb5626ecaa1a
SplatTab <skidm44@gmail.com>
2026-05-30 16:35:35 -0500

    World UI and Font for bank

--- Assets/Scripts/Economy/EconomyManager.cs
+++ Assets/Scripts/Economy/EconomyManager.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class EconomyManager : MonoBehaviour
 {
     private const int CHEESE_GOAL = 10;
-    private int cheeseAmount = 0;
+    public int cheeseAmount = 0;
 
 
     private void OnEnable()
