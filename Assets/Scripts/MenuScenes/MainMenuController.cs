79f3d315632a4b40028d561bd454119bd0523f80
kongshijatwit <JKongShi@gmail.com>
2026-06-06 14:20:41 -0400

    Add credits menu

--- Assets/Scripts/MenuScenes/MainMenuController.cs
+++ Assets/Scripts/MenuScenes/MainMenuController.cs
@@ -3,9 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Camera creditsCamera;
+    [SerializeField] private Canvas mainMenuCanvas;
+    [SerializeField] private Canvas creditsCanvas;
+
+
+    private void Start()
+    {
+        mainCamera.enabled = true;
+        creditsCamera.enabled = false;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void GoToCredits()
+    {
+        mainCamera.enabled = false;
+        creditsCamera.enabled = true;
+        mainMenuCanvas.gameObject.SetActive(false);
+        creditsCamera.gameObject.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        mainCamera.enabled = true;
+        creditsCamera.enabled = false;
+        mainMenuCanvas.gameObject.SetActive(true);
+        creditsCamera.gameObject.SetActive(false);
     }
 
     public void QuitGame()
